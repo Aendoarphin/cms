719c245748ebbc7085dd15a59059b8bce032a28c
aendoarphin <arhon.k.pineda@gmail.com>
2024-11-02 22:52:22 -0500

    add even more stuff

--- server/scripts/user.ts
+++ server/scripts/user.ts
@@ -1,28 +1,52 @@
 import { Firestore, deleteDoc, setDoc, getDoc, doc } from "firebase/firestore";
 
-const addUser = async(db: Firestore, collPath: string, userName: string, userId: string, userPw: string, userEmail: string, userRole: string) => {
-  try {
-    const data = { userName, userId, userPw, userEmail, userRole }
-    getUser(db, collPath, userId)
-    return await setDoc(doc(db, collPath), data)
-  } catch (error) {
-    console.error(error)
-    throw new Error(`Error adding user: ${error}`);
-  }
+interface UserData {
+  userName: string;
+  userId: string;
+  userPw: string;
+  userEmail: string;
+  userRole: string;
 }
 
-const getUser = async(db: Firestore, docPath: string, docId: string) => {
+const addUser = async (db: Firestore, collPath: string, data: UserData) => {
+  const userRef = doc(db, collPath, data.userId);
+  const userSnap = await getDoc(userRef);
+  if (!userSnap.exists()) {
+    await setDoc(userRef, data);
+  } else {
+    console.log("User already exists. Nothing to add.")
+  }
+};
+
+const getUser = async (db: Firestore, docPath: string, docId: string) => {
+  // return user data as object (or undefined if not found)
   const docRef = doc(db, docPath, docId);
   const docSnap = await getDoc(docRef);
-  return docSnap.exists() ? docSnap.data() : {};
-}
+  return docSnap.exists() ? docSnap.data() : undefined;
+};
 
-const setUser = async(db: Firestore, docPath: string, docId: string, data: Object) => {
-  await setDoc(doc(db, docPath, docId), data);
-}
+const setUser = async (
+  db: Firestore,
+  docPath: string,
+  docId: string,
+  data: UserData
+) => {
+  try {
+    const userRef = doc(db, docPath, docId);
+    await setDoc(userRef, data, { merge: true });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-const deleteUser = async(db: Firestore, docPath: string, docId: string) => {
-  await deleteDoc(doc(db, docPath, docId));
-}
+const deleteUser = async (db: Firestore, docPath: string, docId: string) => {
+  const userRef = doc(db, docPath, docId);
+  const userSnap = await getDoc(userRef);
+  if (userSnap.exists()) {
+    await deleteDoc(userRef);
+  } else {
+    console.log("User does not exist. Nothing to delete.")
+  }
+};
 
 export { addUser, getUser, setUser, deleteUser };
