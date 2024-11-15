4039256bd6f14257e0fcbb5a83e3599e6694c28d
aendoarphin <arhon.k.pineda@gmail.com>
2024-11-14 21:18:03 -0600

    add auth functions

--- server/server.ts
+++ server/server.ts
@@ -5,32 +5,32 @@ import { getFirestore } from "firebase/firestore";
 
 import { usersRouter } from "./routes/users";
 import { ticketsRouter } from "./routes/tickets";
-import { authRouter } from "./routes/auth";
 import { rootRouter } from "./routes/root";
-import { addUser, getUser } from "./scripts/user";
+import dotenv from "dotenv";
 
+dotenv.config({path: "../.env"});
 const app = express();
 
 app.use(cors({origin: ["http://localhost:5173"]}));
 app.use(express.json());
+app.use(express.static("public"));
 
 // Routes
 const routes = [
   { path: "/", router: rootRouter },
   { path: "/users", router: usersRouter },
   { path: "/tickets", router: ticketsRouter },
-  { path: "/auth", router: authRouter },
 ];
 routes.forEach((route) => app.use(route.path, route.router));
 
 // Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCh19EXcs-CwO0OaX9A2riD5kpN8de4Mg0",
-  authDomain: "queueit-3fbb9.firebaseapp.com",
-  projectId: "queueit-3fbb9",
-  storageBucket: "queueit-3fbb9.appspot.com",
-  messagingSenderId: "257368545155",
-  appId: "1:257368545155:web:6d170515fc26f6378b08b6",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 const firebaseApp = initializeApp(firebaseConfig);
 export const db = getFirestore(firebaseApp);
