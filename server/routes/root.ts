984b0843c7b7ed2c53adad3334e6bd4787480dd7
Arhon <aronp2000@gmail.com>
2024-12-04 22:55:34 -0600

    edit root.ts

--- server/routes/root.ts
+++ server/routes/root.ts
@@ -1,26 +1,12 @@
 import { Router } from "express";
 import { db } from "../server";
+import { sign } from "jsonwebtoken";
 
 const rootRouter: Router = Router();
 
-rootRouter.get("/", (req, res) => {
-    res.send("GET was sent")
-    console.log("GET was sent")
-});
-
-rootRouter.delete("/logout", (req, res) => {
-    // get reference to sessions collection
-    // get reference to userId
-    // find session and delete it
-})
-
-rootRouter.get("/dashboard", (req, res) => {
-    // get reference to sessions collection
-    // get reference to userId
-    // user session exists
-        // send to dashboard
-    // user session not exists
-        // redirect to login
-});
+rootRouter
+    .get("/", (req, res) => {
+        res.send("GET");
+    })
 
 export { rootRouter }
